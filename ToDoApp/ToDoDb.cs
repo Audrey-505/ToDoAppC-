480339241308846739788a405b10abb3d8ce692c
Audrey Alcantara <audreyalcantara@Audreys-MBP-61.lan>
2024-03-07 16:29:28 -0500

    adding to git

---
+++ ToDoApp/ToDoDb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ToDoApp
+{
+    using Microsoft.EntityFrameworkCore;
+
+    class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options)
+            : base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
+
